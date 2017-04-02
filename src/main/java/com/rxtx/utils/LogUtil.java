af2535aabc11f8e409cf3f552d34c7d7cdf54640
胡同涛 <hutongtao@up366.com>
2017-04-02 13:21:02 +0800

    日志处理

---
+++ src/main/java/com/rxtx/utils/LogUtil.java
@@ -0,0 +1,20 @@
+package com.rxtx.utils;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class LogUtil {
+	
+	public static Logger getLogger(Class<?> cls){
+		return Logger.getLogger(cls);
+	}
+	
+	public static void init(){
+		String base = System.getProperty("user.dir");
+		base += "\\src\\main\\resource\\test";	// 用于debug
+		PropertyConfigurator.configure(base + File.separator + "log4j.properties");
+	}
+}
