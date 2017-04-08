c67e4f9216132d268a4142d90f5c7b411bb0226e
胡同涛 <hutongtao@up366.com>
2017-04-08 13:30:02 +0800

    添加获取端口序列的逻辑处理

--- src/main/java/com/rxtx/logic/WorkLogic.java
+++ src/main/java/com/rxtx/logic/WorkLogic.java
@@ -1,41 +1,57 @@
 package com.rxtx.logic;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
 
 import com.rxtx.comm.Constants;
 import com.rxtx.db.JDBCUtils;
+import com.rxtx.db.WsPortDb;
+import com.rxtx.model.WsPort;
 import com.rxtx.model.WsTask;
 import com.rxtx.model.creater.WsListCreater;
+import com.rxtx.utils.LogUtil;
 
 public class WorkLogic {
-	
-	
-	public void saveData(String rfid,String port){
+
+	private Map<String, WsPort> wsPorts = new HashMap<String, WsPort>();
+	private Logger logger = LogUtil.getLogger(WorkLogic.class);
+
+	public void saveData(String rfid, String port) {
 		int type = checkType(port);
 		List<WsTask> wsTasks = JDBCUtils.getWsTasks(rfid, type);
-		if(wsTasks!=null&&wsTasks.size()>0){
-		WsTask ws = wsTasks.get(0);
-		
-		if(type ==Constants.TYPE_REPAIR){
-			ws.setRepairStationNo(port);
-			ws.setRepairBeginTime(System.currentTimeMillis());
+		if (wsTasks != null && wsTasks.size() > 0) {
+			WsTask ws = wsTasks.get(0);
+
+			if (type == Constants.TYPE_REPAIR) {
+				ws.setRepairStationNo(port);
+				ws.setRepairBeginTime(System.currentTimeMillis());
+			}
+			if (type == Constants.TYPE_WASH) {
+				ws.setWaitBeginTime(System.currentTimeMillis());
+				ws.setWaitStationNo(port);
+			}
+
+			JDBCUtils.update(wsTasks.get(0), type);
 		}
-		if(type==Constants.TYPE_WASH){
-			ws.setWaitBeginTime(System.currentTimeMillis());
-			ws.setWaitStationNo(port);
-		}
-		
-		JDBCUtils.update(wsTasks.get(0), type);
-		}
-		
-		JDBCUtils.add(WsListCreater.createWsList(rfid,port));
+
+		JDBCUtils.add(WsListCreater.createWsList(rfid, port));
 	}
 
 	private int checkType(String port) {
-		if("COM50".equals(port)){
+		if (wsPorts.size() == 0) {
+			wsPorts = WsPortDb.getWsPorts();
+		}
+		WsPort p = wsPorts.get(port);
+		if (p == null) {
+			wsPorts = WsPortDb.getWsPorts();
+			logger.error("Port 没有登记！！！" + port);
 			return 1;
 		}
-		return 2;
+
+		return p.getPortType();
 	}
 
 }
